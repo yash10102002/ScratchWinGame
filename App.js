aaf199d3907b16febef165f0146b904063bbfbb1
unknown <yashkalavadiya1010@gmail.com>
2020-07-19 12:12:28 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,367 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity, Dimensions, Alert } from 'react-native';
+import { Button } from 'native-base';
+import {FontAwesome} from '@expo/vector-icons'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+//get array of length 25
+var itemArray = new Array(25).fill('empty')
+
+
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      randomNumber:"",
+      attemp:0
+    }
+  }
+
+  //when app loads up
+  componentDidMount(){
+    this.setState({randomNumber:this.generateRandomNumber()})
+  }
+
+  generateRandomNumber = () => {
+    let randomNumber = Math.floor(Math.random() * 25)
+    this.setState({ randomNumber, isScratchedd: true })
+    return randomNumber
+  }
+
+  scratchItem = itemNumber => {
+    if(this.state.attemp < 5){
+      if(this.state.randomNumber === itemNumber){
+        itemArray[itemNumber] = "lucky"
+      }else{
+        itemArray[itemNumber] = "unlucky" 
+      }
+      this.setState({attemp: this.state.attemp + 1})
+    }else{
+      Alert.alert("Max Attemp reached")
+    }
+    this.forceUpdate()
+  }
+
+  scratchItemIcon = itemNumber => {
+    if(itemArray[itemNumber] === 'lucky'){
+      return 'dollar'
+    }else if(itemArray[itemNumber] === 'unlucky'){
+      return 'frown-o'
+    }
+    return 'circle'
+  }
+
+  scratchItemColor = itemNumber => {
+    if(itemArray[itemNumber] === 'lucky'){
+      return 'green'
+    }else if(itemArray[itemNumber] === 'unlucky'){
+      return 'red'
+    }
+    return 'black'
+  }
+
+  showAllItem = () => {
+    itemArray.fill('unlucky')
+    itemArray[this.state.randomNumber] = 'lucky'
+    this.setState({attemp:0})
+    this.forceUpdate()
+  }
+
+  resetGame = () => {
+    this.setState({randomNumber: this.generateRandomNumber(), attemp: 0},() => {
+      itemArray.fill('empty')
+      this.forceUpdate()
+    })
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <View style={styles.topbar}>
+          <Text style={styles.topText}>Scratch and Win Game</Text>
+        </View>
+        <View style={styles.grid}>
+          <View style={styles.itemrow}>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(0)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(0)}
+              size={50}
+              color={this.scratchItemColor(0)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(1)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(1)}
+              size={50}
+              color={this.scratchItemColor(1)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(2)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(2)}
+              size={50}
+              color={this.scratchItemColor(2)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(3)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(3)}
+              size={50}
+              color={this.scratchItemColor(3)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(4)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(4)}
+              size={50}
+              color={this.scratchItemColor(4)}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.itemrow}>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(5)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(5)}
+              size={50}
+              color={this.scratchItemColor(5)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(6)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(6)}
+              size={50}
+              color={this.scratchItemColor(6)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(7)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(7)}
+              size={50}
+              color={this.scratchItemColor(7)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(8)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(8)}
+              size={50}
+              color={this.scratchItemColor(8)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(9)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(9)}
+              size={50}
+              color={this.scratchItemColor(9)}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.itemrow}>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(10)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(10)}
+              size={50}
+              color={this.scratchItemColor(10)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(11)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(11)}
+              size={50}
+              color={this.scratchItemColor(11)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(12)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(12)}
+              size={50}
+              color={this.scratchItemColor(12)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(13)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(13)}
+              size={50}
+              color={this.scratchItemColor(13)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(14)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(14)}
+              size={50}
+              color={this.scratchItemColor(14)}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.itemrow}>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(15)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(15)}
+              size={50}
+              color={this.scratchItemColor(15)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(16)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(16)}
+              size={50}
+              color={this.scratchItemColor(16)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(17)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(17)}
+              size={50}
+              color={this.scratchItemColor(17)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(18)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(18)}
+              size={50}
+              color={this.scratchItemColor(18)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(19)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(19)}
+              size={50}
+              color={this.scratchItemColor(19)}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.itemrow}>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(20)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(20)}
+              size={50}
+              color={this.scratchItemColor(20)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(21)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(21)}
+              size={50}
+              color={this.scratchItemColor(21)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(22)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(22)}
+              size={50}
+              color={this.scratchItemColor(22)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(23)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(23)}
+              size={50}
+              color={this.scratchItemColor(23)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.item}
+            onPress={() => this.scratchItem(24)}
+            >
+              <FontAwesome 
+              name={this.scratchItemIcon(24)}
+              size={50}
+              color={this.scratchItemColor(24)}
+              />
+            </TouchableOpacity>
+          </View>
+       
+        </View>
+        <Button
+        full
+        success
+        style={styles.button}
+        onPress={() => {this.showAllItem()}}
+        >
+          <Text style={styles.buttonText}>Show All Coupons</Text>
+        </Button>
+        <Button
+        full
+        primary
+        style={styles.button}
+        onPress={() => {this.resetGame()}}
+        >
+          <Text style={styles.buttonText}>Reset</Text>
+        </Button>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +371,37 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  grid:{
+
+  },
+  itemrow:{
+    flexDirection:'row'
+  },
+  item:{
+    alignItems:'center',
+    padding:10,
+    borderWidth:2,
+    borderColor:'#000',
+    minWidth: 70
+  },
+  button:{
+    marginVertical:15,
+    borderRadius:10
+  },
+  buttonText:{
+    color:'#FFF',
+    fontSize:20
+  },
+  topbar:{
+    backgroundColor:'#8B78E6',
+    height:50,    
+    justifyContent:'center',
+    width:Dimensions.get('window').width,
+    alignItems:'center',
+    marginVertical:20
+  },
+  topText:{
+    color:'#FFF',
+    fontSize:20
+  }
 });
---
+++ README.md
@@ -0,0 +1,33 @@
+<h1 align="center">Welcome to ScratchWin 👋</h1>
+<p>
+  <a href="https://twitter.com/yashkalavadiya1" target="_blank">
+    <img alt="Twitter: yashkalavadiya1" src="https://img.shields.io/twitter/follow/yashkalavadiya1.svg?style=social" />
+  </a>
+</p>
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+```sh
+npm run start
+```
+
+## Author
+
+👤 **Yash Kalavadiya**
+
+* Twitter: [@yashkalavadiya1](https://twitter.com/yashkalavadiya1)
+* Github: [@yash10102002](https://github.com/yash10102002)
+* LinkedIn: [@yash kalavadiya](https://linkedin.com/in/yash kalavadiya)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
